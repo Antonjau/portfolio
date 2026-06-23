71b9e405db6ca8ead3ded5279f934739c1c46445
Antonjau <130472003+Antonjau@users.noreply.github.com>
2026-06-23 15:07:25 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase project config for Anthony Njau's portfolio
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDVfWCCyCGfbVhw3sQNIOTkYapfo7B9B1M",
+  authDomain: "portfolio-management-ed2d5.firebaseapp.com",
+  projectId: "portfolio-management-ed2d5",
+  storageBucket: "portfolio-management-ed2d5.firebasestorage.app",
+  messagingSenderId: "307935862567",
+  appId: "1:307935862567:web:9a3dce523d134c96bbd302",
+  measurementId: "G-X797LW4YQB"
+};
+
+firebase.initializeApp(firebaseConfig);
